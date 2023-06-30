d3b29704c33c6d38e5661a2bc101b7a97b40f59c
William Edmund Mironchuk <wmironchuk@gmail.com>
2023-06-30 16:46:18 -0400

    Refactoring done
    the queue is now created in a factory method

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from collections import deque, namedtuple
 row = list[str]
 grid = list[row]
 
+def __generate_starting_queue() -> deque:
+    mainQueue = deque()
+    for y,array in enumerate(board):
+        for x,letter in enumerate(array):
+            if letter == word[:1]:
+                deque.appendleft((x,y))
+    return mainQueue
+
 def exists(board: grid, word: str) -> bool:
     """
     This method is intended to take in a grid of letters
     and then tell me if there is a path through it that spells out
     the word desired
     """
-    mainQueue = deque()
-    for y,array in enumerate(board):
-        for x,letter in enumerate(array):
-            if letter == word[:1]:
-                deque.appendleft((x,y))
+    mainQueue = __generate_starting_queue()
